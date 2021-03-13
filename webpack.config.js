891fbe144ea66b0543aea01949dc8f9f6299f07c
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-13 15:41:04 -0300

    fast refresh no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 //uso de variáveis de ambiente
 const isDevelopment = process.env.NODE_ENV !== "production";
@@ -19,20 +20,29 @@ module.exports = {
   devServer: {
     //listener de alterações no código, automatiza a conversão do webpack e babel
     contentBase: path.resolve(__dirname, "public"),
+    hot: true,
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       //injeta arquivo js no html, melhora o fluxo da aplicação
       template: path.resolve(__dirname, "public", "index.html"),
     }),
-  ],
+  ].filter(Boolean),
   module: {
     //regras de como a aplicação deve se comportar com diferentes importações nos files
     rules: [
       {
         test: /\.jsx$/, //verificar se esse file é desse formato ou não
         exclude: /node_modules/, //cada biblioteca deve se preocupar com seu processo de build
-        use: "babel-loader", //integração entre webpack e babel: webpack identifica e babel converte;
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        }, //integração entre webpack e babel: webpack identifica e babel converte;
       },
       {
         test: /\.scss$/,
