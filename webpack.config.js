351a2b77b372ad805d803a4e89a675a3a8d707cd
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-13 13:56:22 -0300

    ambiente dev & prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+//uso de variáveis de ambiente
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 module.exports = {
-  mode: "development",
-  devtoolt: "eval-source-map", //facilita a visualização do código pelo debugger no browser
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map", //facilita a visualização do código pelo debugger no browser
   entry: path.resolve(__dirname, "src", "index.jsx"), //file principal da aplicação
   output: {
     //file de saída com código convertido entendido pelo browser
