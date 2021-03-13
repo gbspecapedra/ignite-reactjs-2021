5e54c85671fe39434cd461faf77ea1d9e3623333
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-13 13:27:13 -0300

    configurando webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,3 @@
 import React from "react";
+import { App } from './App'
 
-export function index() {
-  return <h1>Hello World</h1>;
-}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.jsx"), //file principal da aplicação
+  output: {
+    //file de saída com código convertido entendido pelo browser
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], //arquivos lidos para funcionamento da aplicação
+  },
+  module: {
+    //regras de como a aplicação deve se comportar com diferentes importações nos files
+    rules: [
+      {
+        test: /\.jsx$/, //verificar se esse file é desse formato ou não
+        exclude: /node_modules/, //cada biblioteca deve se preocupar com seu processo de build
+        use: "babel-loader", //integração entre webpack e babel: webpack identifica e babel converte;
+      },
+    ],
+  },
+};
