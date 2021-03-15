fd9561295c9e68d7e82fc96b814ed21e545d4a04
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-15 19:18:02 -0300

    instal styled-components

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
+
+const Title = styled.h1`
+  color: #8257e6;
+`;
