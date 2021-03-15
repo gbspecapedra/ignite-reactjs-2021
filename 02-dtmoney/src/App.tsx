4ff43c83d32a3bd9db1709657e9c48145f095fb3
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-15 19:43:06 -0300

    criando estilos globais

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,13 +1,10 @@
-import styled from "styled-components";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
-
-const Title = styled.h1`
-  color: #8257e6;
-`;
