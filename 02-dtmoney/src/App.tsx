80a6fb7a62739dae9779cb7883ec3bed912be6a6
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-21 11:01:48 -0300

    Context API no React

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -20,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -30,6 +31,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
--- 02-dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { api } from "../../services/api";
+import { TransactionsContext } from "../../TransactionsContext";
+
 import { Container } from "./styles";
 
 interface Transaction {
@@ -12,6 +14,7 @@ interface Transaction {
 }
 
 export function TransactionsTable() {
+  const data = useContext(TransactionsContext);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
