e804c1bd42292b15a8a74c791684c0b2dec788a8
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-23 17:01:30 -0300

    estilos globais

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   );
 }
