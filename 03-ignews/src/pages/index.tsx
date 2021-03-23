efe1d4dec7b9268068e1b0e3516a6ee8f62a11d1
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-23 16:51:01 -0300

    title dinamico por pagina

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from "next/head";
+
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  );
 }
