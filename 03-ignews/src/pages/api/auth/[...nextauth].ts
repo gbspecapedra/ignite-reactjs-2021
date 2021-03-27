13caeed09988d595b43b5ae59c830fff3ebfbbfb
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-27 16:09:34 -0300

    salvando usuario no banco

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,11 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+      return true;
+    },
+  },
 });
