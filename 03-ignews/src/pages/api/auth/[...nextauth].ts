386d7a8e4db3d77fe63f87cc12373f4946cad133
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-03-27 16:14:22 -0300

    verificando usuario duplicado

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -15,8 +15,22 @@ export default NextAuth({
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user;
-      await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
-      return true;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
     },
   },
 });
