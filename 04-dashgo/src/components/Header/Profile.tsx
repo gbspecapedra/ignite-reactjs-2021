3500bf9d6a767355a54a7b09bdf31b2e288c384c
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-04-04 09:53:27 -0300

    header responsivo

--- 04-dashgo/src/components/Header/Profile.tsx
+++ 04-dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gisele Bernardes</Text>
-        <Text color="gray.300" fontSize="small">
-          giiselebernardes@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gisele Bernardes</Text>
+          <Text color="gray.300" fontSize="small">
+            giiselebernardes@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
