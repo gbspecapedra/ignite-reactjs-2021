df19b634afaa34c8a6464eabb0a0085ee11efa73
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-04-17 10:16:40 -0300

    configurando miragejs

--- 04-dashgo/src/pages/_app.tsx
+++ 04-dashgo/src/pages/_app.tsx
@@ -3,7 +3,12 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 
+import { makeServer } from "../services/mirage";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
