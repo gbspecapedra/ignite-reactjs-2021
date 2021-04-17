658b903353943cc3a67a9886da70c8b69c0519f3
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-04-17 12:17:00 -0300

    configurando devTools

--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -23,25 +23,31 @@ import { Header, Sidebar } from "../../components";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
 
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
 
-    return users;
-  });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
