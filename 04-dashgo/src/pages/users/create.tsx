d53531218433fcf5e90a60fe9a338bed6690806a
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-04-18 14:27:48 -0300

    utilizando mutations

--- 04-dashgo/src/pages/users/create.tsx
+++ 04-dashgo/src/pages/users/create.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "react-query";
 import {
   Box,
   Button,
@@ -8,10 +14,9 @@ import {
   SimpleGrid,
   VStack,
 } from "@chakra-ui/react";
-import Link from "next/link";
-import { SubmitHandler, useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 import { Header, Sidebar } from "../../components";
 import { Input } from "../../components/Form";
@@ -36,6 +41,26 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
+
   const {
     register,
     handleSubmit,
@@ -47,7 +72,8 @@ export default function CreateUser() {
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
     values
   ) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await createUser.mutateAsync(values);
+    router.push("/users");
   };
 
   return (
