966cfce46bba8d052e545fe528e3646e226bba4a
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-04-18 12:58:38 -0300

    criando hook de listagem

--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -19,36 +19,13 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 import { Header, Sidebar } from "../../components";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("users");
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    }
-  );
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
