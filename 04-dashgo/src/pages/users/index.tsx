b15d662a1adb8b58e699c1800d25a6bfc49dbc0b
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-04-18 12:19:31 -0300

    configurando axios

--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -19,6 +19,8 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
+import { api } from "../../services/api";
+
 import { Header, Sidebar } from "../../components";
 import { Pagination } from "../../components/Pagination";
 
@@ -26,8 +28,7 @@ export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("users");
 
       const users = data.users.map((user) => {
         return {
