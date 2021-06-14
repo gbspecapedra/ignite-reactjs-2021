a436f1e3533f24237b3b243c17926d75d0c35f97
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-06-14 20:07:29 -0300

    recuperando estado da autenticacao

--- 05-nextauth/pages/dashboard.tsx
+++ 05-nextauth/pages/dashboard.tsx
@@ -1,8 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { api } from "../services/api";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get("/me").then((response) => console.log(response));
+  });
 
   return (
     <div>
