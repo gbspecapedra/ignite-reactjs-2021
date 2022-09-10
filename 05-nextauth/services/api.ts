699b27dffe1061fc7f5d006a0e808178c41fef96
Gisele Pecapedra <gisele.bernardes@winedirect.com>
2022-09-10 16:06:40 -0300

    redirecionamento pelo servidor

--- 05-nextauth/services/api.ts
+++ 05-nextauth/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
 import { signOut } from "../contexts/AuthContext";
-
+import { AuthTokenError } from './errors/AuthTokenError';
 
 let isRefreshing = false;
 let failedRequestQueue = [];
@@ -82,6 +82,8 @@ export function setupAPIClient(ctx = undefined) {
         } else {
           if (process.browser) {
             signOut();
+          } else {
+            return Promise.reject(new AuthTokenError())
           }
         }
       }
--- 05-nextauth/utils/withSSRAuth.ts
+++ 05-nextauth/utils/withSSRAuth.ts
@@ -1,5 +1,6 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
-import { parseCookies } from 'nookies';
+import { destroyCookie, parseCookies } from 'nookies';
+import { AuthTokenError } from './../services/errors/AuthTokenError';
 
 // High Order Function
 export function withSSRAuth<T>(fn: GetServerSideProps<T>): GetServerSideProps {
@@ -15,6 +16,21 @@ export function withSSRAuth<T>(fn: GetServerSideProps<T>): GetServerSideProps {
       };
     }
 
-    return await fn(ctx)
+    try {
+      return await fn(ctx)
+    } catch (error) {
+      if (error instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token')
+        destroyCookie(ctx, 'nextauth.refreshToken')
+
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          }
+        }
+      }
+    }
+
   }
 }
