5874ec9817892f6f0975e50782e75539a751be94
Gisele Pecapedra <gisele.bernardes@winedirect.com>
2022-09-10 17:30:49 -0300

    broadcast de logout

--- 05-nextauth/pages/dashboard.tsx
+++ 05-nextauth/pages/dashboard.tsx
@@ -7,7 +7,7 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   const userCanSeeMetrics = useCan({ permissions: ["metrics.list"] });
 
@@ -21,6 +21,7 @@ export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard: {user?.email}</h1>
+      <button onClick={signOut}>Sign Out</button>
 
       {userCanSeeMetrics && <h2>Metrics</h2>}
 
